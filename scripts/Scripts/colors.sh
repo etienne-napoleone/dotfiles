03ab1a88a2091e377e269b5579c28efccc0127a3
n0wataken <n0@waifu.club>
2017-09-26 01:08:54 +0200

    dmenu config

---
+++ scripts/Scripts/colors.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+Black='\033[0;30m'
+Dark_Gray='\033[1;30m'
+Red='\033[0;31m'
+Light_Red='\033[1;31m'
+Green='\033[0;32m'
+Light_Green='\033[1;32m'
+Brown='\033[0;33m'
+Yellow='\033[1;33m'
+Blue='\033[0;34m'
+Light_Blue='\033[1;34m'
+Purple='\033[0;35m'
+Light_Purple='\033[1;35m'
+Cyan='\033[0;36m'
+Light_Cyan='\033[1;36m'
+Light_Gray='\033[0;37m'
+White='\033[1;37m'
+NC='\033[0m'
+
+#one line
+# echo -e "${Black}█${Red}█${Green}█${Brown}█${Blue}█${Purple}█${Cyan}█${Light_Gray}█${NC}"
+# echo -e "${Dark_Gray}█${Light_Red}█${Light_Green}█${Yellow}█${Light_Blue}█${Light_Purple}█${Light_Cyan=}█${White}█${NC}"
+echo -e "\n"
+echo -e "${Black}█${Black}█${Black}█ ${Red}█${Red}█${Red}█ ${Green}█${Green}█${Green}█ ${Brown}█${Brown}█${Brown}█ ${Blue}█${Blue}█${Blue}█ ${Purple}█${Purple}█${Purple}█ ${Cyan}█${Cyan}█${Cyan}█ ${Light_Gray}█${Light_Gray}█${Light_Gray}█ ${NC}"
+echo -e "${Black}█${Black}█${Black}█ ${Red}█${Red}█${Red}█ ${Green}█${Green}█${Green}█ ${Brown}█${Brown}█${Brown}█ ${Blue}█${Blue}█${Blue}█ ${Purple}█${Purple}█${Purple}█ ${Cyan}█${Cyan}█${Cyan}█ ${Light_Gray}█${Light_Gray}█${Light_Gray}█ ${NC}"
+echo -e "${Dark_Gray}█${Dark_Gray}█${Dark_Gray}█ ${Light_Red}█${Light_Red}█${Light_Red}█ ${Light_Green}█${Light_Green}█${Light_Green}█ ${Yellow}█${Yellow}█${Yellow}█ ${Light_Blue}█${Light_Blue}█${Light_Blue}█ ${Light_Purple}█${Light_Purple}█${Light_Purple}█ ${Light_Cyan}█${Light_Cyan}█${Light_Cyan}█ ${White}█${White}█${White}█ ${NC}"
+echo -e "\n"
